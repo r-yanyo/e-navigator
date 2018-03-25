2f2cb2de983bcfb14c2405619e9f0ebb5a35d90f
r-yanyo <honma.ryosuke@gmail.com>
2018-03-25 19:27:51 +0900

    面接の編集と削除機能を追加

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -15,4 +15,42 @@ class InterviewsController < ApplicationController
       end
     end
   end
+
+  def new
+    @user = current_user
+    @interview = Interview.new()
+  end
+
+  def create
+    @interview = Interview.new(interview_params)
+    @interview.acceptance = "保留"
+    if @interview.save
+      redirect_to interviews_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = current_user
+    @interview = Interview.find_by(id: params[:id])
+  end
+
+  def update
+    @interview = Interview.find_by(id: params[:id])
+    if @interview.update_attributes(interview_params)
+      redirect_to interviews_path
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    Interview.find_by(id: params[:id]).destroy
+    redirect_to interviews_path
+  end
+
+  def interview_params
+    params.require(:interview).permit(:date, :acceptance, :user_id)
+  end
 end
--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,4 @@
 class Interview < ApplicationRecord
   belongs_to :user
+  enum acceptance:{"保留": 0,"承認": 1, "却下": 2}
 end
