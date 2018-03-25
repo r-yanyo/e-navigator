e2dd282486b26b3ccb46ced8351bc7b5e0ddfa65
honma <honma@ty.webimpact.co.jp>
2018-03-25 19:27:51 +0900

    面接の編集と削除機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "users/registrations"
   }
+  get '/interviews/new', to: 'interviews#new'
+  post '/interviews/create', to: 'interviews#create'
+  get '/interviews/edit/:id', to: 'interviews#edit'
+  put '/interviews/update/:id', to: 'interviews#update'
+  post '/interviews/delete/:id', to: 'interviews#delete'
+  
   get '/interviews/:id', to: 'interviews#index'
-  get '/interviews', to: 'interviews#index'  
+  get '/interviews', to: 'interviews#index'
   resources :users, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
