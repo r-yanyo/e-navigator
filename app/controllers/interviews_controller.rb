b6b6292e7912d1b811cfc75d0c3187116aed19c0
honma <honma@ty.webimpact.co.jp>
2018-03-25 16:17:24 +0900

    他人の面接一覧を見れるように変更

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -2,7 +2,17 @@ class InterviewsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @user = current_user
-    @interviews = @user.interviews
+    id = params[:id]
+    if id == nil
+      @user = current_user
+      @interviews = @user.interviews
+    else
+      @user = User.find_by(id: id)
+      if @user
+        @interviews = @user.interviews
+      else
+        render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+      end
+    end
   end
 end
